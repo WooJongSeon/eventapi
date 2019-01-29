473c857e2f9c5711173027edfdf84caf5bda0535
WooJongSeon <jswoo030@gmail.com>
2019-01-30 00:18:28 +0900

    errors serializer 추가

--- src/test/java/com/example/api/events/EventControllerTests.java
+++ src/test/java/com/example/api/events/EventControllerTests.java
@@ -16,6 +16,7 @@ import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 import java.time.LocalDateTime;
+import java.util.*;
 
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultHandlers.print;
